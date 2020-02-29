656ff9fe8b33f66f7f0d09a0063701b0dc0d998f
jie_cao <jie_cao@ctrip.com>
2020-02-29 15:20:17 +0800

    update

--- src/main/java/com/cjppo/backend/war/Application.java
+++ src/main/java/com/cjppo/backend/war/Application.java
@@ -10,10 +10,14 @@ import org.springframework.web.bind.annotation.RestController;
 @EnableAsync
 @RestController
 @SpringBootApplication
-public class Application {
+public class Application extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(Application.class);
+    }
 }
