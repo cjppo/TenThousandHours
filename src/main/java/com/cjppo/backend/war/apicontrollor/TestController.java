395e8851a38f80159d4bd94b4c6488287ca62091
Sakuya <cjkl1317@163.com>
2020-05-15 15:25:46 +0800

    Update TestController.java

--- src/main/java/com/cjppo/backend/war/apicontrollor/TestController.java
+++ src/main/java/com/cjppo/backend/war/apicontrollor/TestController.java
@@ -1,5 +1,6 @@
 package com.cjppo.backend.war.apicontrollor;
 
+import org.springframework.web.bind.annotation;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
